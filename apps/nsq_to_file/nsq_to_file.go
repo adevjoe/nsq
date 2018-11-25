8be3e200dbf615c576c0950512ec17f73843515f
joe <adevjoe@gmail.com>
2018-11-25 21:59:03 +0800

    Add: go mod file and some gitignore

--- apps/nsq_to_file/nsq_to_file.go
+++ apps/nsq_to_file/nsq_to_file.go
@@ -18,6 +18,40 @@ import (
 	"github.com/nsqio/nsq/internal/version"
 )
 
+var (
+	showVersion = flag.Bool("version", false, "print version string")
+
+	channel     = flag.String("channel", "nsq_to_file", "nsq channel")
+	maxInFlight = flag.Int("max-in-flight", 200, "max number of messages to allow in flight")
+
+	outputDir      = flag.String("output-dir", "/tmp", "directory to write output files to")
+	datetimeFormat = flag.String("datetime-format", "%Y-%m-%d_%H", "strftime compatible format for <DATETIME> in filename format")
+	filenameFormat = flag.String("filename-format", "<TOPIC>.<HOST><REV>.<DATETIME>.log", "output filename format (<TOPIC>, <HOST>, <PID>, <DATETIME>, <REV> are replaced. <REV> is increased when file already exists)")
+	hostIdentifier = flag.String("host-identifier", "", "value to output in log filename in place of hostname. <SHORT_HOST> and <HOSTNAME> are valid replacement tokens")
+	gzipLevel      = flag.Int("gzip-level", 6, "gzip compression level (1-9, 1=BestSpeed, 9=BestCompression)")
+	gzipEnabled    = flag.Bool("gzip", false, "gzip output files.")
+	skipEmptyFiles = flag.Bool("skip-empty-files", false, "Skip writing empty files")
+	topicPollRate  = flag.Duration("topic-refresh", time.Minute, "how frequently the topic list should be refreshed")
+	topicPattern   = flag.String("topic-pattern", "", "Only log topics matching the following pattern")
+
+	rotateSize     = flag.Int64("rotate-size", 0, "rotate the file when it grows bigger than `rotate-size` bytes")
+	rotateInterval = flag.Duration("rotate-interval", 0*time.Second, "rotate the file every duration")
+	syncInterval   = flag.Duration("sync-interval", 30*time.Second, "sync file to disk every duration")
+
+	httpConnectTimeout = flag.Duration("http-client-connect-timeout", 2*time.Second, "timeout for HTTP connect")
+	httpRequestTimeout = flag.Duration("http-client-request-timeout", 5*time.Second, "timeout for HTTP request")
+
+	nsqdTCPAddrs     = app.StringArray{}
+	lookupdHTTPAddrs = app.StringArray{}
+	topics           = app.StringArray{}
+)
+
+func init() {
+	flag.Var(&nsqdTCPAddrs, "nsqd-tcp-address", "nsqd TCP address (may be given multiple times)")
+	flag.Var(&lookupdHTTPAddrs, "lookupd-http-address", "lookupd HTTP address (may be given multiple times)")
+	flag.Var(&topics, "topic", "nsq topic (may be given multiple times)")
+}
+
 func hasArg(s string) bool {
 	argExist := false
 	flag.Visit(func(f *flag.Flag) {
--- go.mod
+++ go.mod
@@ -1,19 +1,20 @@
 module github.com/nsqio/nsq
 
 require (
-	github.com/BurntSushi/toml v0.3.1
-	github.com/bitly/go-hostpool v0.1.0
-	github.com/bitly/timer_metrics v1.0.0
-	github.com/blang/semver v3.5.1+incompatible
-	github.com/bmizerany/perks v0.0.0-20141205001514-d9a9656a3a4b
+	github.com/BurntSushi/toml v0.0.0-20130929175405-2dff11163ee6
+	github.com/bitly/go-hostpool v0.0.0-20171023180738-a3a6125de932
+	github.com/bitly/timer_metrics v0.0.0-20150203034119-afad1794bb13
+	github.com/blang/semver v2.1.0+incompatible
+	github.com/bmizerany/perks v0.0.0-20140429223840-6cb9d9d72930
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/golang/snappy v0.0.1
-	github.com/judwhite/go-svc v1.1.2
-	github.com/julienschmidt/httprouter v1.3.0
-	github.com/mreiferson/go-options v1.0.0
-	github.com/nsqio/go-diskqueue v1.0.0
-	github.com/nsqio/go-nsq v1.0.8
-	golang.org/x/sys v0.0.0-20191224085550-c709ea063b76 // indirect
+	github.com/golang/snappy v0.0.0-20160529050041-d9eb7a3d35ec
+	github.com/judwhite/go-svc v1.0.0
+	github.com/julienschmidt/httprouter v0.0.0-20150708215400-6aacfd5ab513
+	github.com/mreiferson/go-options v0.0.0-20161229190002-77551d20752b
+	github.com/nsqio/go-diskqueue v0.0.0-20170827071139-0681a1afee12
+	github.com/nsqio/go-nsq v0.0.0-20161229185821-a53d495e8142
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.2.2 // indirect
+	golang.org/x/sys v0.0.0-20181122145206-62eef0e2fa9b // indirect
+	golang.org/x/tools v0.0.0-20181122213734-04b5d21e00f1 // indirect
 )
-
-go 1.13
